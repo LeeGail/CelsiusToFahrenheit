6b2da216da9a07da9e327b03b70bbde64e4683fd
Leegail <gleervc@gmail.com>
2018-09-28 12:09:18 -0500

    9/27/18 - created project for first Assignment

---
+++ src/celsiustofahrenheit/CelsiusToFahrenheit.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package celsiustofahrenheit;
+
+/**
+ *
+ * @author Gail Lee
+ */
+import java.util.Scanner;
+public class CelsiusToFahrenheit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    float Celsius;
+    double Fahrenheit;
+    Scanner scanner = new Scanner(System.in);
+    
+            
+        System.out.print("Enter your Celsius Temperature:");
+        Celsius = scanner.nextFloat();
+        Fahrenheit = (9.0/5.0*Celsius)+32;
+                
+    System.out.print("Your Farenheight number ="+Fahrenheit);
+    }
+}
